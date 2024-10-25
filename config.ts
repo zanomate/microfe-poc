6f238529ef8836a3975781cd2145a5699b4043a9
Matteo Zanoncello <matteozanoncello@milkmantechnologies.com>
2024-10-25 15:26:34 +0200

    add protocols to domains

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
-export const CONTAINER_DOMAIN = 'microfe-poc-container.netlify.app'
-export const REMOTE_DOMAIN = 'microfe-poc-remote.netlify.app'
+export const CONTAINER_DOMAIN = 'https://microfe-poc-container.netlify.app'
+export const REMOTE_DOMAIN = 'https://microfe-poc-remote.netlify.app'
 
 export const CONTAINER_PORT = 3001
 export const REMOTE_PORT = 3002
